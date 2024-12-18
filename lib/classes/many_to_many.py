75e1235cf3d87c14978609868d6cf03e8b6ab3bb
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-12-18 01:43:12 -0500

    updated core again

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -85,7 +85,7 @@ class Magazine:
         return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
-        titles= list(set([article.title for article in self.articles()]))
+        titles= [article.title for article in self.articles()]
         return titles if titles else None
     def contributing_authors(self):
      contributing_authors = [
